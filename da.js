60f0508435cad87e1178c88078a45d30a25dc372
ntorbinskiy <n.torbinskiy@gmail.com>
2022-05-05 14:51:29 +0300

    descr

---
+++ da.js
@@ -0,0 +1,4 @@
+let da = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel tenetur, ut repellat voluptatem eligendi temporibus voluptatum fugit quas, deserunt iste cupiditate. Molestias natus voluptate placeat nam sit minima odit laborum.";
+console.log(da.length);
+da = da.slice(0, 216) + "...";
+console.log(da);
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -8,18 +8,28 @@ class MarvelService {
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
-    
         return await res.json();
     }
     
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters =  async () => {
+        const res = await this.getResource(`${this._apiBase}?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}/${id}?${this._apiKey}`)
+    getCharacter =  async (id) => {
+        const res = await this.getResource(`${this._apiBase}/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
     }
 
+    _transformCharacter = (char) => {
+    return {
+        name: char.name,
+        description: char.description ? `${char.description.slice(0,210)}...` : "This character hasn't got any description",
+        thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+        homepage: char.urls[0].url,
+        wiki: char.urls[1].url
+    }
+}
 }
 
 export default MarvelService;
